60614fa92b93ce1d1c9acebb936ee6931d8f10c8
vikvikash27 <vikvikash27@gmail.com>
2024-02-19 16:14:14 +0530

    message

---
+++ components/MainPage/SampleCard.tsx
@@ -0,0 +1,107 @@
+import React from "react";
+import Image from "next/image";
+
+const SampleCard = () => {
+  return (
+    <div>
+      <section className="relative pt-16 bg-blueGray-50">
+        <div className="container mx-auto">
+          <div className="flex flex-wrap items-center">
+            <div className="w-10/12 md:w-6/12 lg:w-4/12 px-12 md:px-4 mr-auto ml-auto -mt-78">
+              <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-700">
+                <Image
+                  src={"/gallery/image-1.png"}
+                  width={360}
+                  height={240}
+                  alt="Text"
+                  className="w-full align-middle rounded-t-lg"
+                />
+                <blockquote className="relative p-8 mb-4">
+                  <svg
+                    preserveAspectRatio="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 583 95"
+                    className="absolute left-0 w-full block h-95-px -top-94-px"
+                  >
+                   
+                  </svg>
+                  <h4 className="text-xl font-bold text-white">
+                    Viper Code Lab
+                  </h4>
+                  <p className="text-md font-light mt-2 text-white">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Corporis suscipit tempora maxime quia eveniet eligendi
+                    explicabo rerum iure soluta facere asperiores itaque,
+                    inventore possimus quibusdam?.
+                  </p>
+                </blockquote>
+              </div>
+            </div>
+
+            <div className="w-full md:w-6/12 px-4">
+              <div className="flex flex-wrap">
+                <div className="w-full md:w-6/12 px-4">
+                  <div className="relative flex flex-col mt-4">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-sitemap"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">
+                        Tailwind Components
+                      </h6>
+                      <p className="mb-4 text-blueGray-500">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      </p>
+                    </div>
+                  </div>
+                  <div className="relative flex flex-col min-w-0">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-drafting-compass"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">
+                        JavaScript Components
+                      </h6>
+                      <p className="mb-4 text-blueGray-500">
+                        We also feature many dynamic components for React,
+                        NextJS, Vue and Angular.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="w-full md:w-6/12 px-4">
+                  <div className="relative flex flex-col min-w-0 mt-4">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-newspaper"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">Pages</h6>
+                      <p className="mb-4 text-blueGray-500">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      </p>
+                    </div>
+                  </div>
+                  <div className="relative flex flex-col min-w-0">
+                    <div className="px-4 py-5 flex-auto">
+                      <div className="text-blueGray-500 p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-white">
+                        <i className="fas fa-file-alt"></i>
+                      </div>
+                      <h6 className="text-xl mb-1 font-semibold">
+                        Documentation
+                      </h6>
+                      <p className="mb-4 text-blueGray-500">
+                        Viper Code Lab, Lorem ipsum dolor sit amet, consectetur adipisicing.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default SampleCard;
